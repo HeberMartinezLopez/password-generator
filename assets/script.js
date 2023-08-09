3c033e2f1ee12e4892142f1997d803b9e14508eb
Heber Martinez Lopez <hebermartinez14@gmail.com>
2023-08-08 23:52:54 -0700

    add functions to js that generate letters, nums, special char

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,105 @@
 // Assignment code here
 
+// generate prompt to obtain user input for password length
+function passwordLen(){
+  var passLen = prompt("Choose a password length of at least 8 characters long and no more than 128 characters.");
+  
+  // input validation
+  if(passLen < 8 || passLen > 128){
+    alert("Please enter a number greater than 8 characters and less than 128 characters.");
+    passwordLen();
+  }
+    // error prompt
+  else{
+    return console.log(passLen);
+  }
+}
+// generate prompt to obtain user input if lowercase letters is desired
+function incLower(){
+  var lowerChar = prompt("Would you like you include lowercase characters? Enter 'Y' for yes 'N' for no");
+
+  // input validation
+  if(lowerChar === "y" || lowerChar === "Y"){
+    return (String.fromCharCode(Math.floor(Math.random() * 26) + 97));
+  }
+  else if(lowerChar === "n" || lowerChar === "N"){
+    return console.log(lowerChar);
+
+  }
+    // error prompt
+  else{
+    alert("Invalid input.");
+    incLower();
+  }
+}
+
+// generate prompt to obtain user input if upper case letter are desired
+function incUpper(){
+  var upperChar = prompt("Would you like you include uppercase characters? Enter 'Y' for yes 'N' for no");
+
+  // input validation 
+  if(upperChar === "y" || upperChar === "Y"){
+    return (String.fromCharCode(Math.floor(Math.random() * 26) + 65));
+  }
+  else if(upperChar === "n" || upperChar === "N"){
+    return console.log(upperChar);
+  }
+    // error prompt
+  else{
+    alert("Invalid input.");
+    incUpper();
+  }
+}
+
+// generate prompt to obtain user input if numbers desired
+function incNum(){
+  var numChar = prompt("Would you like you include numeric characters? Enter 'Y' for yes 'N' for no");
+
+  // input validation
+  if(numChar === "y" || numChar === "Y"){
+    return (String.fromCharCode(Math.floor(Math.random() * 10) + 48));
+  }
+  else if(numChar === "n" || numChar === "N"){
+    return console.log(numChar);
+  }
+    // error prompt
+  else{
+    alert("Invalid input.");
+    incNum();
+  }
+}
+
+// generate prompt to obtain user input if special characters desired
+function incSpec(){
+  var specChar = prompt("Would you like you include special characters? Enter 'Y' for yes 'N' for no");
+
+  // input validation
+  if(specChar === "y" || specChar === "Y"){
+    var symbols = '!@#$%^&*(){}[]<>=/,.';
+    return symbols [Math.floor(Math.random() * symbols.length)];
+  }
+  else if(specChar === "n" || specChar === "N"){
+    return console.log(specChar);
+  }
+  // error prompt
+  else{
+    alert("Invalid input.");
+    incSpec();
+  }
+
+}
+function generatePassword(){
+  passwordLen();
+  incLower();
+  incUpper();
+  incNum();
+  incSpec();
+
+  // var pwGen = '';
+  // for( var i = 0; i < length; i++){
+
+  // }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +115,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
