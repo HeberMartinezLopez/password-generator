74675de475fad59992e4ab96999ca412928ca63d
Heber Martinez Lopez <hebermartinez14@gmail.com>
2023-08-10 18:32:08 -0700

    redo functions to generate password

--- assets/script.js
+++ assets/script.js
@@ -2,103 +2,88 @@
 
 // generate prompt to obtain user input for password length
 function passwordLen(){
-  var passLen = prompt("Choose a password length of at least 8 characters long and no more than 128 characters.");
+
+  var passLen = parseInt(prompt("Choose a password length of at least 8 characters long and no more than 128 characters."));
   
   // input validation
-  if(passLen < 8 || passLen > 128){
+  if(passLen < 8 || passLen > 128 || isNaN(passLen)){
+
     alert("Please enter a number greater than 8 characters and less than 128 characters.");
-    passwordLen();
-  }
-    // error prompt
-  else{
-    return console.log(passLen);
-  }
-}
-// generate prompt to obtain user input if lowercase letters is desired
-function incLower(){
-  var lowerChar = prompt("Would you like you include lowercase characters? Enter 'Y' for yes 'N' for no");
-
-  // input validation
-  if(lowerChar === "y" || lowerChar === "Y"){
-    return (String.fromCharCode(Math.floor(Math.random() * 26) + 97));
-  }
-  else if(lowerChar === "n" || lowerChar === "N"){
-    return console.log(lowerChar);
+    return passwordLen();
 
   }
-    // error prompt
-  else{
-    alert("Invalid input.");
-    incLower();
-  }
+  
+  return passLen;
+
 }
 
-// generate prompt to obtain user input if upper case letter are desired
-function incUpper(){
-  var upperChar = prompt("Would you like you include uppercase characters? Enter 'Y' for yes 'N' for no");
+function includeChars(getCharMsg){
+  
+  var count = 0;
+  var userInput = prompt(getCharMsg).toLowerCase();
 
-  // input validation 
-  if(upperChar === "y" || upperChar === "Y"){
-    return (String.fromCharCode(Math.floor(Math.random() * 26) + 65));
+  if(userInput === "y"){
+    
+    return true;
+
+  } 
+  else if (userInput === "n"){
+    
+    count++;
+    console.log("count: ", count);
+
+    return false;
+  
+  
   }
-  else if(upperChar === "n" || upperChar === "N"){
-    return console.log(upperChar);
-  }
-    // error prompt
+  
   else{
-    alert("Invalid input.");
-    incUpper();
-  }
-}
 
-// generate prompt to obtain user input if numbers desired
-function incNum(){
-  var numChar = prompt("Would you like you include numeric characters? Enter 'Y' for yes 'N' for no");
+    alert("Invalid input. Please enter 'Y' or 'N'.");
+    return includeChars(getCharMsg);
 
-  // input validation
-  if(numChar === "y" || numChar === "Y"){
-    return (String.fromCharCode(Math.floor(Math.random() * 10) + 48));
-  }
-  else if(numChar === "n" || numChar === "N"){
-    return console.log(numChar);
-  }
-    // error prompt
-  else{
-    alert("Invalid input.");
-    incNum();
-  }
-}
-
-// generate prompt to obtain user input if special characters desired
-function incSpec(){
-  var specChar = prompt("Would you like you include special characters? Enter 'Y' for yes 'N' for no");
-
-  // input validation
-  if(specChar === "y" || specChar === "Y"){
-    var symbols = '!@#$%^&*(){}[]<>=/,.';
-    return symbols [Math.floor(Math.random() * symbols.length)];
-  }
-  else if(specChar === "n" || specChar === "N"){
-    return console.log(specChar);
-  }
-  // error prompt
-  else{
-    alert("Invalid input.");
-    incSpec();
   }
 
 }
+
 function generatePassword(){
-  passwordLen();
-  incLower();
-  incUpper();
-  incNum();
-  incSpec();
 
-  // var pwGen = '';
-  // for( var i = 0; i < length; i++){
+  var passLen = passwordLen();
+  var incLower = includeChars("Would you like you include lowercase characters? Enter 'Y' for yes 'N' for no");
+  var incUpper = includeChars("Would you like you include uppercase characters? Enter 'Y' for yes 'N' for no");
+  var incNum = includeChars("Would you like you include numeric characters? Enter 'Y' for yes 'N' for no");
+  var incSpec = includeChars("Would you like you include special characters? Enter 'Y' for yes 'N' for no");
 
-  // }
+  var lower = 'abcdefghijklmnopqrstuvwxyz';
+  var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var numbers = '0123456789';
+  var symbols = '!@#$%^&*(){}[]<>=/,.';
+  var allChars = '';
+  
+  if(incLower) {
+    allChars += lower;
+  }
+  if(incUpper) {
+    allChars += upper;
+  }
+  if(incNum) {
+    allChars += numbers;
+  }
+  if(incSpec) {
+    allChars += symbols;
+  }
+  
+  var generatedPw = '';
+  for (var i = 0; i < passLen; i++){
+
+    var randomIndex = Math.floor(Math.random() * allChars.length);
+    generatedPw += allChars[randomIndex];
+  
+  }
+  
+
+  return generatedPw;
+
 }
 
 // Get references to the #generate element
@@ -111,8 +96,8 @@ function writePassword() {
 
   passwordText.value = password;
 
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
